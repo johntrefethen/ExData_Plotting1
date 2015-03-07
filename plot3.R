12f83ed1d2e6897ab7d68ba85a2e4707f3695f0f
John Trefethen <trefethens@gmail.com>
2015-03-07 07:46:32 -0700

    Remove box around legend

--- plot3.R
+++ plot3.R
@@ -43,7 +43,8 @@ legend("topright",
        c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), 
        lty=c(1,1), 
        lwd=c(1,1),
-       col=c("black","blue","red"))
+       col=c("black","blue","red"),
+       bty = "n")
 
 # Close the device
 dev.off()
