7fa20001748117c8031757fea533529e9cd64e6a
John Trefethen <trefethens@gmail.com>
2015-03-07 07:47:48 -0700

    Add back legend
    Required for this individual graph!

--- plot3.R
+++ plot3.R
@@ -43,8 +43,7 @@ legend("topright",
        c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), 
        lty=c(1,1), 
        lwd=c(1,1),
-       col=c("black","blue","red"),
-       bty = "n")
+       col=c("black","blue","red"))
 
 # Close the device
 dev.off()
